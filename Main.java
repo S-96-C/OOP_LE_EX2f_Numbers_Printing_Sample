767da4c626871b98fed2b8272d756fcb471210ce
Shehan Cooray <109017782+S-96-C@users.noreply.github.com>
2022-08-13 23:07:51 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    
+	int no = 1;
+	while(no < 10){
+		System.out.println(no);
+		no++;
+	}
+	System.out.println(no);
+
+  }
+}
